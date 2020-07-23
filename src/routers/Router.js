9c1b24fb4dd59fc569059d3d570501e1f69b8d94
leminhhieu19 <62608311+leminhhieu19@users.noreply.github.com>
2020-07-23 18:18:38 +0700

    Update Router.js

--- src/routers/Router.js
+++ src/routers/Router.js
@@ -7,17 +7,17 @@ import RegisterScreen from '../screens/RegisterScreen';
 import ForgotScreen from '../screens/ForgotScreen';
 import WbnPlayer from '../components/containers/WbnPlayer';
 import View from '../screens/View';
+
 const Router = () => (
     <>
         <BrowserRouter basename="/react_videoplayer/">
             <Header />
                 <Switch>
-                <Route exact path="/:activeVideo" component={WbnPlayer} />
-                <Route exact path="/todos/view/:id" component={View} />
+                    <Route exact path="/:activeVideo" component={WbnPlayer} />
+                    <Route exact path="/todos/view/:id" component={View} />
                     <Route exact path="/auths/login" component={LoginScreen} />
                     <Route exact path="/auths/register" component={RegisterScreen} />
                     <Route exact path="/auths/forgotpassword" component={ForgotScreen} />
-                   
                 </Switch>
             <GlobalStyle />
         </BrowserRouter>
