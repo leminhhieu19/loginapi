20c2778fe54e3eec9d55d74f4d0c2c254967a3b6
leminhhieu19 <62608311+leminhhieu19@users.noreply.github.com>
2020-07-23 18:16:57 +0700

    Update Header.js

--- src/screens/Header.js
+++ src/screens/Header.js
@@ -1,15 +1,37 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import StyledHeader from '../components/styles/StyledHeader';
 import { Link } from "react-router-dom";
-
+import Modal from 'react-modal';
+import LoginScreen from './LoginScreen';
+import RegisterScreen from './RegisterScreen';
 const Header = () => {
-
+    const [showModal, toggleModal] = useState(false)
+    const [showRegisterModal, registerModal] = useState(false)
     return (
         <StyledHeader>
             <div className="header">
                 <div className="header-right">
-                    <Link className="" to="/auths/login">Login</Link>
-                    <Link className="" to="/auths/register">Register</Link>
+                    <Link className="" onClick={() => toggleModal(true)} >Login</Link>
+                    <Modal className="modal1"
+                        parentSelector={() => document.querySelector('#root')}
+                        closeTimeoutMS={2}
+                        isOpen={showModal}
+                        contentLabel="modal"
+                        onRequestClose={() => toggleModal(false)}
+                    >
+                        <LoginScreen onClick={() => toggleModal(false) } />
+                    </Modal>
+                    
+                    
+                    <Link className="" onClick={() => registerModal(true)}>Register</Link>
+                    <Modal className="modal2"
+                        parentSelector={() => document.querySelector('#root')}
+                        closeTimeoutMS={200}
+                        isOpen={showRegisterModal}
+                        contentLabel="modal"
+                        onRequestClose={() => registerModal(false)}>
+                            <RegisterScreen onClick={()=> registerModal(true)} />
+                        </Modal>
                 </div>
             </div>
         </StyledHeader >
