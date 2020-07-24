c8f526c20b430651229cf15e9be88bb78236bb9e
leminhhieu19 <62608311+leminhhieu19@users.noreply.github.com>
2020-07-24 17:20:07 +0700

    Update Header.js

--- src/screens/Header.js
+++ src/screens/Header.js
@@ -1,40 +1,102 @@
-import React, {useState} from 'react';
+import React, {useState, memo} from 'react';
 import StyledHeader from '../components/styles/StyledHeader';
 import { Link } from "react-router-dom";
 import Modal from 'react-modal';
 import LoginScreen from './LoginScreen';
 import RegisterScreen from './RegisterScreen';
-const Header = () => {
-    const [showModal, toggleModal] = useState(false)
+Modal.setAppElement('#root')
+const Header = memo(function() {
+    const [showModal, toggleModal] = useState(true)
     const [showRegisterModal, registerModal] = useState(false)
     return (
         <StyledHeader>
             <div className="header">
                 <div className="header-right">
-                    <Link className="" onClick={() => toggleModal(true)} >Login</Link>
+                    <Link onClick={() => toggleModal(true)} >Login</Link>
                     <Modal className="modal1"
-                        parentSelector={() => document.querySelector('#root')}
+                        
                         closeTimeoutMS={2}
                         isOpen={showModal}
                         contentLabel="modal"
                         onRequestClose={() => toggleModal(false)}
+                        style={{
+                            
+                            overlay: {
+                            overflow: 'hidden',
+                              position: 'fixed',
+                              top: 0,
+                              left: 0,
+                              right: 0,
+                              bottom: 0,
+                              backgroundColor: 'rgba(255, 255, 255, 0.75)'
+                            },
+                            content: {
+                              position: 'absolute',
+                              top: '40px',
+                              left: '40px',
+                              right: '40px',
+                              bottom: '40px',
+                              border: '1px solid #ccc',
+                              background: '#fff',
+                              overflow: 'auto',
+                              WebkitOverflowScrolling: 'touch',
+                              borderRadius: '4px',
+                              outline: 'none',
+                              padding: '20px'
+                            }
+                            
+                          }}
                     >
-                        <LoginScreen onClick={() => toggleModal(false) } />
+                            <div className='modal-footer'>
+                                <Link onClick={() =>toggleModal(false)}>Close</Link>
+                             </div>
+                        <LoginScreen onClick={() => toggleModal(true) } />
+                        
                     </Modal>
                     
                     
-                    <Link className="" onClick={() => registerModal(true)}>Register</Link>
+                    <Link onClick={() => registerModal(true)}>Register</Link>
                     <Modal className="modal2"
-                        parentSelector={() => document.querySelector('#root')}
+
                         closeTimeoutMS={200}
                         isOpen={showRegisterModal}
                         contentLabel="modal"
-                        onRequestClose={() => registerModal(false)}>
+                        onRequestClose={() => registerModal(false)}
+                        style={{
+                            
+                            overlay: {
+                            overflow: 'hidden',
+                              position: 'fixed',
+                              top: 0,
+                              left: 0,
+                              right: 0,
+                              bottom: 0,
+                              backgroundColor: 'rgba(255, 255, 255, 0.75)'
+                            },
+                            content: {
+                              position: 'absolute',
+                              top: '40px',
+                              left: '40px',
+                              right: '40px',
+                              bottom: '40px',
+                              border: '1px solid #ccc',
+                              background: '#fff',
+                              overflow: 'auto',
+                              WebkitOverflowScrolling: 'touch',
+                              borderRadius: '4px',
+                              outline: 'none',
+                              padding: '20px'
+                            }
+                            
+                          }}>
+                              <div className='modal-footer'>
+                                <Link onClick={() =>registerModal(false)}>Close</Link>
+                             </div>
                             <RegisterScreen onClick={()=> registerModal(true)} />
                         </Modal>
                 </div>
             </div>
         </StyledHeader >
     );
-};
+});
 export default Header;
