73723277a48aff09514ea3727338353ec39ee2ef
leminhhieu19 <62608311+leminhhieu19@users.noreply.github.com>
2020-07-29 18:19:43 +0700

    Update LoginScreen.js

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,97 +1,62 @@
-import React, { useState, useEffect, memo } from "react";
+import React, { useState } from "react";
 import StyledLoginRegister from "../components/styles/StyledLoginRegister";
-import { Link } from "react-router-dom";
-import { Redirect } from 'react-router-dom'
-import services from "../services/Api";
+import { Link, Redirect} from "react-router-dom";
+import axios from "axios";
 
-const LoginScreen = memo( function() {
-
-    let loggedIn = true;
-
-    const token = localStorage.getItem("token")
-    if (token == null) {
-        loggedIn = false
-    }
+const LoginScreen = () => {
     const [user, setUser] = useState({
-        id: "",
-        username: "",
-        password: "",
-        loggedIn
+        userName: '',
+        password: '',
+        login: false,
+        token: '',
     });
-    const { username, password } = user;
-
-    //load user api
-    const [users, setUsers] = useState([]);
-
-    useEffect(() => {
-        loadUsers();
-    }, []);
-
-    const loadUsers = async () => {
-        const result = await services.getUserById();
-        setUsers(result.data.reverse());
-    };
-    //
+    const { userName, password } = user;
     const onChange = e => {
-        setUser({ ...user, [e.target.name]: e.target.value });
+        setUser({...user, [e.target.name]: e.target.value });
     };
-
     const submitForm = e => {
-        e.preventDefault()
-        try {
-            if (password === "" || username === "") {   
-                alert('You need to fill in all the information!!!')
-                return true;
-            }
-            console.log(username)
-            console.log(password)
-
-            users.forEach(user => {
-                if (username === user.username && password === user.password) {
-                    localStorage.setItem("token", "123456789")
-                    setUser({
-                        id: user.id,
-                        loggedIn: true
-                    })
-                }
-            });
-        } catch (error) {
-            console.log(error)
-           alert("Register failed");
-        }
-
-    };
-    if (loggedIn) {
-        return <Redirect to={`/todos/view/${user.id}`} />
-    };
-    
-    return (       
+        e.preventDefault();
+        axios.post(`https://milky-auth.herokuapp.com/login`, {
+            userName: userName,
+            password: password,
+          
+        }).then(res => {
+            localStorage.setItem('example-jwt-jwt', res.data.result.access_token);
+          //   props.history.push({
+          //     pathname: `http://localhost:3003/users`,
+          // })
+        });
+        
+    }
+    return (
         <StyledLoginRegister>
             <div className="wrapper">
                 <div className="title">
                     Login
-                </div>
+            </div>
                 <form onSubmit={e => submitForm(e)}>
                     <div className="form">
                         <div className="inputfield">
                             <label>Username</label>
-                            <input type="text" class="input" name="username" value={username} onChange={e => onChange(e)}  />
+                            <input type="text" className="input" name="userName" value={userName} onChange={e => onChange(e)} />
                         </div>
                         <div className="inputfield">
                             <label>Password</label>
-                            <input type="password" className="input" name="password" value={password} onChange={e => onChange(e)} />
+                            <input type="password" className="input" name="password" value={password} onChange={e => onChange(e)}/>
                         </div>
                         <div className="inputfield">
-                            <input type="submit" value="Login" className="btn"/>
+                            <input type="submit" value="Login" className="btn" />
                         </div>
                     </div>
                 </form>
+
                 <div className="forgot">
-                    <Link to="/auths/forgotpassword">Forgot password</Link>
+                    <Link className="" to="/auths/forgotpassword">Forgot password</Link>
+          
                 </div>
             </div>
         </StyledLoginRegister >
-    );
-})
+    )
+}
 
 export default LoginScreen;
