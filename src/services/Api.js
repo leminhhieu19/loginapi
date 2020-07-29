9481f39c6ffd5ae7552b146bbf8b19aaf5a92ab0
leminhhieu19 <62608311+leminhhieu19@users.noreply.github.com>
2020-07-29 18:20:22 +0700

    Update Api.js

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default {
     addUser: (user) =>
-        axios.post("http://localhost:3003/users", user),
+        axios.post("https://milky-auth.herokuapp.com/login", user),
     getUserById: () =>
-        axios.get("http://localhost:3003/users"),
+        axios.get("https://milky-auth.herokuapp.com/login"),
 }
